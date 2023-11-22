1003b519c47fd4dff9c6e3ae50a6e01fd68f38e9
beingshahbaj <130038256+beingshahbaj@users.noreply.github.com>
2023-11-22 11:52:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 //your code here!
+const scrollContainer = document.getElementById("infi-list");
+let count =1;
+for(; count<=10; count++) {
+  const li = document.createElement("li");
+  li.textContent = `item `;
+  li.className = "item";
+  scrollContainer.appendChild(li);
+};
 
+scrollContainer.addEventListener("scroll" ,()=>{
+  let scrollHeight = scrollContainer.scrollTop;
+  let scrolableHeight = scrollContainer.scrollHeight - scrollContainer.clientHeight;
+  if(scrolableHeight - scrollHeight <= 5){
+    addTwoMore();
+  }
+});
+
+function addTwoMore(){
+  for(let i = count; i<=count+2; i++){
+    const li = document.createElement("li");
+    li.textContent = ` item`;
+    li.className = "item";
+    scrollContainer.appendChild(li);
+  
+  }
+}
